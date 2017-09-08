bf49e1d04763deaa4ca8f3bae0a25e8959098182
Alexander Biskop <abiskop@netflix.com>
2017-09-08 16:20:41 -0700

    Add setInterval that publishes mock events

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const create = options => {
     const queueHandlers = [];
     const broadcastHandlers = [];
 
+    let mockEventInterval = null;
+
     const onEvent = (handler) => {
         queueHandlers.push(handler);
+
+        if (!mockEventInterval) {
+            mockEventInterval = setInterval(() => {
+                const timestamp = new Date().getTime();
+                // Simulate dummy event
+                const dummyEvent = {
+                    namespace: 'npp',
+                    operation: 'create',
+                    entity: {
+                        id: `id_${timestamp}`,
+                        type: 'someEntityType',
+                        data: { timestamp }
+                    }
+                };
+                enqueue(dummyEvent);
+            }, 5000);
+        }
     };
     const onMessage = (handler) => {
         broadcastHandlers.push(handler);
